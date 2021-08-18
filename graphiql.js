062f55c0325737db3f263a0b7cb141c30b623e5b
Jamie Barton <jamie@notrab.dev>
2021-08-18 16:49:15 +0100

    revert: graphiql

--- graphiql.js
+++ graphiql.js
@@ -1,30 +1,69 @@
 exports.graphiqlHtml = ({ path }) => `<!DOCTYPE html>
 <html>
   <head>
-    <title>GraphiQL</title>
-    <link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
+    <style>
+      body {
+        height: 100%;
+        margin: 0;
+        width: 100%;
+        overflow: hidden;
+      }
+      #graphiql {
+        height: 100vh;
+      }
+    </style>
+    <!--
+      This GraphiQL example depends on Promise and fetch, which are available in
+      modern browsers, but can be "polyfilled" for older browsers.
+      GraphiQL itself depends on React DOM.
+      If you do not want to rely on a CDN, you can host these files locally or
+      include them directly in your favored resource bunder.
+    -->
+    <script
+      crossorigin
+      src="https://unpkg.com/react@16/umd/react.development.js"
+    ></script>
+    <script
+      crossorigin
+      src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"
+    ></script>
+    <!--
+      These two files can be found in the npm module, however you may wish to
+      copy them directly into your environment, or perhaps include them in your
+      favored resource bundler.
+     -->
+    <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
   </head>
-  <body style="margin: 0;">
-    <div id="graphiql" style="height: 100vh;"></div>
-
+  <body>
+    <div id="graphiql">Loading...</div>
     <script
-      crossorigin
-      src="https://unpkg.com/react/umd/react.production.min.js"
-    ></script>
-    <script
-      crossorigin
-      src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-    ></script>
-    <script
-      crossorigin
       src="https://unpkg.com/graphiql/graphiql.min.js"
+      type="application/javascript"
     ></script>
-
     <script>
-      const fetcher = GraphiQL.createFetcher('${path}');
-
+      function graphQLFetcher(graphQLParams) {
+        return fetch(
+          '${path}',
+          {
+            method: 'post',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(graphQLParams),
+            credentials: 'omit',
+          },
+        ).then(function (response) {
+          return response.json().catch(function () {
+            return response.text();
+          });
+        });
+      }
       ReactDOM.render(
-        React.createElement(GraphiQL, { fetcher: fetcher }),
+        React.createElement(GraphiQL, {
+          fetcher: graphQLFetcher,
+          defaultVariableEditorOpen: true,
+        }),
         document.getElementById('graphiql'),
       );
     </script>
