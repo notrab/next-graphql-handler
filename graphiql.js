a12404004400c0f218e8af304b2baeb8a2cddf5b
Jamie Barton <jamie@notrab.dev>
2021-08-18 16:26:16 +0100

    chore: move out graphiql to file

---
+++ graphiql.js
@@ -0,0 +1,32 @@
+export const graphiqlHtml = ({ path }) => `<!DOCTYPE html>
+<html>
+  <head>
+    <title>GraphiQL</title>
+    <link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
+  </head>
+  <body style="margin: 0;">
+    <div id="graphiql" style="height: 100vh;"></div>
+
+    <script
+      crossorigin
+      src="https://unpkg.com/react/umd/react.production.min.js"
+    ></script>
+    <script
+      crossorigin
+      src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+    ></script>
+    <script
+      crossorigin
+      src="https://unpkg.com/graphiql/graphiql.min.js"
+    ></script>
+
+    <script>
+      const fetcher = GraphiQL.createFetcher('${path}');
+
+      ReactDOM.render(
+        React.createElement(GraphiQL, { fetcher: fetcher }),
+        document.getElementById('graphiql'),
+      );
+    </script>
+  </body>
+</html>`;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { graphql } from "graphql";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
+import { graphiqlHtml } from "./graphiql";
+
 export const GraphQLHandler = ({ typeDefs, resolvers, ...options }) => {
   const schema = makeExecutableSchema({
     typeDefs,
@@ -11,83 +13,7 @@ export const GraphQLHandler = ({ typeDefs, resolvers, ...options }) => {
     const { method } = req;
 
     if (method === "GET") {
-      return res.status(200).send(`<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      body {
-        height: 100%;
-        margin: 0;
-        width: 100%;
-        overflow: hidden;
-      }
-
-      #graphiql {
-        height: 100vh;
-      }
-    </style>
-
-    <!--
-      This GraphiQL example depends on Promise and fetch, which are available in
-      modern browsers, but can be "polyfilled" for older browsers.
-      GraphiQL itself depends on React DOM.
-      If you do not want to rely on a CDN, you can host these files locally or
-      include them directly in your favored resource bunder.
-    -->
-    <script
-      crossorigin
-      src="https://unpkg.com/react@16/umd/react.development.js"
-    ></script>
-    <script
-      crossorigin
-      src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"
-    ></script>
-
-    <!--
-      These two files can be found in the npm module, however you may wish to
-      copy them directly into your environment, or perhaps include them in your
-      favored resource bundler.
-     -->
-    <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
-  </head>
-
-  <body>
-    <div id="graphiql">Loading...</div>
-    <script
-      src="https://unpkg.com/graphiql/graphiql.min.js"
-      type="application/javascript"
-    ></script>
-    <script src="/renderExample.js" type="application/javascript"></script>
-    <script>
-      function graphQLFetcher(graphQLParams) {
-        return fetch(
-          '/api/graphql',
-          {
-            method: 'post',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(graphQLParams),
-            credentials: 'omit',
-          },
-        ).then(function (response) {
-          return response.json().catch(function () {
-            return response.text();
-          });
-        });
-      }
-
-      ReactDOM.render(
-        React.createElement(GraphiQL, {
-          fetcher: graphQLFetcher,
-          defaultVariableEditorOpen: true,
-        }),
-        document.getElementById('graphiql'),
-      );
-    </script>
-  </body>
-</html>`);
+      return res.status(200).send(graphiqlHtml({ path: "/api/graphql" }));
     }
 
     const { query, variables, operationName } = req.body;
